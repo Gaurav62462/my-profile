a221cc01dabb2fa333d68de3ea9b0b97fe56129c
Balwinder <balwinder.singh829@gmail.com>
2025-04-30 14:20:11 +0530

    js change

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,14 +1,13 @@
 // lib/mongoose.ts
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const cached = (global as any).mongoose || { conn: null, promise: null };
+const cached = global.mongoose || { conn: null, promise: null };
 
 export async function connectToDB() {
   if (cached.conn) return cached.conn;
@@ -16,6 +15,8 @@ export async function connectToDB() {
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       dbName: 'my_portfolio',
+      serverSelectionTimeoutMS: 3000,
+      socketTimeoutMS: 4500,
     }).then((mongoose) => mongoose);
   }
 
