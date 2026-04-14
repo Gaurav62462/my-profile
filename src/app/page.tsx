0251964115f8dd462d306c7a0e04d98d6f92c965
Balwinder <balwinder.singh829@gmail.com>
2026-04-14 18:53:43 +0530

    api changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,16 @@ import Resume from "@/components/Resume";
 import Skills from "@/components/Skills";
 // import TestComponent from "@/components/TestComponent";
 import { Divider } from "@/constant/CommonFunction";
-import { IpData } from "@/constant/types";
-// import { useCheckVisitor } from "@/hooks/useCheckVisitor";
+import { useCheckVisitor } from "@/hooks/useCheckVisitor";
 import { useEffect } from "react";
 
 export default function Home() {
-  // const {checkVisiter} = useCheckVisitor()
+  const {checkVisiter} = useCheckVisitor()
 
 
   useEffect(() => {
     try {
-      // const ref = typeof document !== 'undefined' ? document.referrer : '';
-        const visitUrl = document.referrer;
-        const ref = visitUrl || window.location.href;
+      const ref = typeof document !== 'undefined' ? document.referrer : '';
       if (ref) {
         setTimeout(() => {
           checkVisiter(ref);
@@ -37,52 +34,6 @@ export default function Home() {
     }
 
   }, []);
-
-  const ipUrl = process.env.NEXT_PUBLIC_GET_IP_DETAILS || '';
-  
-    async function checkVisiter(visitUrl: string) {
-      const controller = new AbortController();
-      const timeout = setTimeout(() => controller.abort(), 8000);
-      const url = "/api/visit-user";
-  
-      try {
-        const res = await fetch(ipUrl);
-        const data: IpData = await res.json();
-  
-        const locRes = {
-          ip: data.ip,
-          city: data.city,
-          region: data.region,
-          country_name: data.country_name,
-          org: data.org,
-          network: data.network,
-          latitude: data.latitude,
-          longitude: data.longitude,
-          timezone: data.timezone,
-          utc_offset: data.utc_offset,
-          asn: data.asn,
-        };
-  
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ visiterUrl: visitUrl, locRes: locRes }),
-          signal: controller.signal,
-        });
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        return json
-      } catch (error) {
-        console.error(error);
-      } finally {
-        clearTimeout(timeout);
-      }
-  
-    }
 
   //   const callApi = async () => {
   //   try {
